6a2273becac17b9c3c724ca63d8c170003c65a8a
KinzaImran <kinzaimran30@gmail.com>
2015-09-18 17:46:18 +0000

    initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+def create
+ @article = Article.find(params[:article_id])
+ @comment = @article.comments.create(comment_params)
+ #redirect_to article_path(@article)
+
+ respond_to do |format|
+  format.html redirect_to @article
+  format.js #render comments/create.js.erb
+ end
+end
+ 
+def destroy
+ @article = Article.find(params[:article_id])
+ @comment = @article.comments.find(params[:id])
+ @comment.destroy
+ redirect_to articles_path
+end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end
